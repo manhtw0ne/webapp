03e38057bb8429cf438a96453a9a41c62d56ff5e
manhtw0ne <manh.tw0ne@gmail.com>
2025-11-22 14:26:51 +0700

    integrated react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import Navbar from "./components/Navbar"
 import Dashboard from "./pages/dashboard/Dashboard"
+import ExpenseDetails from "./pages/expense/ExpenseDetails.tsx"
+import ExpenseReports from "./pages/expense/ExpenseReports.tsx"
+import NewExpense from "./pages/expense/NewExpense.tsx"
+import Login from "./pages/login/Login.tsx"
+import Register from "./pages/register/Register.tsx"
 
 
 const App = () => {
-    return <Dashboard/>
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Dashboard  />}/>
+                <Route path="/login" element={<Login  />}/>
+                <Route path="/register" element={<Register  />}/>
+                <Route path="/new" element={<NewExpense  />}/>
+                <Route path="/view" element={<ExpenseDetails  />}/>
+                <Route path="/reports" element={<ExpenseReports  />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
