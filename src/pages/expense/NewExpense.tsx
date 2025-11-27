301f4069a4dc7b0c9740cefe8ee1dd866cdc2248
manhtw0ne <manh.tw0ne@gmail.com>
2025-11-27 16:05:31 +0700

    save expense completed

--- src/pages/expense/NewExpense.tsx
+++ src/pages/expense/NewExpense.tsx
@@ -1,7 +1,88 @@
+import {useFormik} from "formik";
+import { Expense } from "../../model/Expense";
+import expenseValidationSchema from "../../validation/expenseValidationSchema";
+import Dropdown from "../../components/Dropdown";
+import { expenseCategories } from "../../utils/AppConstans";
+import { saveOrUpdateExpense } from "../../services/expense-service";
+import { useState } from "react";
+
+
+
+const expenseValidationSchema = Yup.object({
+  name: Yup.string().required("Expense name is required")
+})
+
 const NewExpense = () => {
+  const [error, setErrors] = useState<string>("");
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: 0,
+      note: "",
+      category: "",
+      date: new Date().toISOString().split("T")[0]
+    },
+    onSubmit: (values: Expense) => {
+
+      saveOrUpdateExpense(values)
+      .then(response => console.log(response)
+    ).catch(error => {console.log(error);
+      setErrors(error.message);
+
+    });
+
+    },
+    validationSchema: expenseValidationSchema,
+  });
   return (
-    <div>
-      
+    <div className="d-flex justify-content-center align-items-center mt-2">
+      {error && <p>{error}</p>}
+      <div className="container col-md-4 col-sm-8 col-xs-12">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input type="text" id="name" name="name" className="form-control" value={formik.values.name}  onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            {formik.touched.name && formik.errors.name ? (
+              <div className="text-danger fst-italic">{formik.errors.name}</div>
+            ) : null}
+            <div className="text-danger fst-italic">
+              Expense name is required
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="amount" className="form-label">
+              Amount
+            </label>
+            <input type="number" id="amount" name="amount" className="form-control" value={formik.values.amount} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            {formik.touched.amount && formik.errors.amount ? <div className="text-danger fst-italic">
+              {formik.errors.amount}
+            </div> : null}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="note" className="form-label">
+              Note
+            </label>
+            <textarea id="note" name="note" className="form-control" value={formik.values.note} onChange={formik.handleChange}/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="date" className="form-label">
+              Date
+            </label>
+            <input type="date" id="date" name="date" className="form-control" value={formik.values.date} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            {formik.touched.date && formik.errors.date ? (<div className="text-danger fst-italic">
+              {} </div>) : null}
+          </div>
+              
+            
+        
+          <Dropdown options={expenseCategories} id="category" name="category" label="Category" value={formik.values.category} onChange={formik.handleChange} onBlur={formik.handleBlur} error={formik.errors.category} touched={formik.touched.category}/>
+          <button className="btn btn-sm app-primary-bg-color btn-outline-light" type="submit">
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
